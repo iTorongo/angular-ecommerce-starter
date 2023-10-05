aece6f769139bd9fd916646f98cb8f6dba9c08f5
Arnaf Aziz Torongo <arnafaziz@gmail.com>
2023-10-05 23:35:49 +0200

    feat: add product type

---
+++ src/app/core/types/product.types.ts
@@ -0,0 +1,13 @@
+export type Product = {
+  id: number;
+  title: string;
+  price: number;
+  brand: string;
+  category: string;
+  description: string;
+  discountPercentage: number;
+  images: string[];
+  rating: number;
+  stock: number;
+  thumbnail: string;
+};
