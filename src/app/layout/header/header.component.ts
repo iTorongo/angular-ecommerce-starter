403ea2788fda6cd5f90a4ccb7b0792897e82c0bb
Arnaf Aziz Torongo <arnafaziz@gmail.com>
2023-10-06 00:09:49 +0200

    feat: add cart icon in header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBars,
+  faXmark,
+  faCartShopping,
+} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -8,6 +12,8 @@ import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 export class HeaderComponent {
   faBars = faBars;
   faXmark = faXmark;
+  faCartShopping = faCartShopping;
+
   menuOpen = false;
 
   toggleMenu(): void {
