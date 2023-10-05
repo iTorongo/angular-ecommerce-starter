fb9d809bd72f401dc5303dfb9e94450ed00917f4
Arnaf Aziz Torongo <arnafaziz@gmail.com>
2023-10-05 22:17:41 +0200

    feat: add header component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
 import { DefaultLayoutComponent } from './default-layout/default-layout.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [DefaultLayoutComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [DefaultLayoutComponent],
+  declarations: [DefaultLayoutComponent, HeaderComponent],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
+  exports: [DefaultLayoutComponent, HeaderComponent],
 })
 export class LayoutModule {}
