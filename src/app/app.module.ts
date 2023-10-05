8cae05ffe3d040ac7548a336f17f9346fdeba2fd
Arnaf Aziz Torongo <arnafaziz@gmail.com>
2023-10-05 23:24:01 +0200

    chore: add shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 import { AppRoutingRoutingModule } from './app-routing.module';
 import { LayoutModule } from './layout/layout.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, LayoutModule, AppRoutingRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    CoreModule,
+    LayoutModule,
+    SharedModule,
+    AppRoutingRoutingModule,
+    FontAwesomeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule, FontAwesomeModule],
+  exports: [],
+})
+export class SharedModule {}
