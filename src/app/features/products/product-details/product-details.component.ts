e2659df1be21ab010f432f0301d582c53e60c3a9
Arnaf Aziz Torongo <arnafaziz@gmail.com>
2023-10-06 18:36:06 +0200

    feat: add product details screen

--- src/app/features/products/product-details/product-details.component.ts
+++ src/app/features/products/product-details/product-details.component.ts
@@ -1,8 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ProductsService } from '../products.service';
+import { faArrowLeftLong, faStar } from '@fortawesome/free-solid-svg-icons';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.scss'],
 })
-export class ProductDetailsComponent {}
+export class ProductDetailsComponent implements OnInit {
+  faArrowLeftLong = faArrowLeftLong;
+  faStar = faStar;
+  public product$: Observable<any> = new Observable();
+
+  /**
+   * Request Id
+   */
+  productId!: number;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService,
+    private location: Location
+  ) {}
+
+  ngOnInit() {
+    this.productId = parseInt(
+      this.route.snapshot.paramMap.get('productId') ?? '',
+      10
+    );
+    if (this.productId) {
+      this.getProduct();
+    }
+  }
+
+  getProduct() {
+    this.product$ = this.productsService.getProduct(this.productId);
+  }
+
+  addToCart() {
+    console.log('Product added to cart');
+  }
+
+  backTo() {
+    this.location.back();
+  }
+}
