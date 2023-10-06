09bcf1932c867764969e34fcc8ec7950414d91bc
Arnaf Aziz Torongo <arnafaziz@gmail.com>
2023-10-06 13:17:56 +0200

    feat: implement truncate pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TruncatePipe } from './pipes/truncate.pipe';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeroSectionComponent, ProductCardComponent } from './components/';
 import { ButtonComponent } from '../standalone/components';
@@ -7,7 +8,7 @@ import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [HeroSectionComponent, ProductCardComponent],
+  declarations: [TruncatePipe, HeroSectionComponent, ProductCardComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
@@ -16,6 +17,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     FontAwesomeModule,
     ButtonComponent,
   ],
-  exports: [FormsModule, HeroSectionComponent, ProductCardComponent],
+  exports: [
+    FormsModule,
+    TruncatePipe,
+    HeroSectionComponent,
+    ProductCardComponent,
+  ],
 })
 export class SharedModule {}
