0dcfd4fa53cafcc7918c96f26ffac8adbc694833
Arnaf Aziz Torongo <arnafaziz@gmail.com>
2023-10-06 12:18:37 +0200

    feat: add home page hero section component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TruncatePipe } from './pipes/truncate.pipe';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HeroSectionComponent } from './components/';
+import { ButtonComponent } from '../standalone/components';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SortPipe } from './pipes/sort.pipe';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, ReactiveFormsModule, RouterModule, FontAwesomeModule],
-  exports: [],
+  declarations: [HeroSectionComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    FontAwesomeModule,
+    ButtonComponent,
+  ],
+  exports: [HeroSectionComponent],
 })
 export class SharedModule {}
