b8b257cca55e783ef4af9fc4ed31a2fe24c7963e
Arnaf Aziz Torongo <arnafaziz@gmail.com>
2023-10-06 13:22:28 +0200

    feat: implement product list screen

--- src/app/features/products/product-list/product-list.component.ts
+++ src/app/features/products/product-list/product-list.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
+import { Observable } from 'rxjs';
+import { SortBy } from '***REMOVED***app/core/enums/sort.enum';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss'],
 })
-export class ProductListComponent {}
+export class ProductListComponent implements OnInit {
+  public products$: Observable<any> = new Observable();
+  fakeProducts = Array(12);
+
+  constructor(private productService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.products$ = this.productService.getProducts();
+  }
+}
