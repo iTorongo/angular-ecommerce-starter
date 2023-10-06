52873bc9fa5454a2f2628bd1842660a7247f5af2
Arnaf Aziz Torongo <arnafaziz@gmail.com>
2023-10-07 00:29:45 +0200

    feat: add cart count in header

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
 import { DefaultLayoutComponent } from './default-layout/default-layout.component';
 import { IconComponent } from '../standalone/components/icon/icon.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [DefaultLayoutComponent, HeaderComponent],
-  imports: [CommonModule, RouterModule, IconComponent],
+  imports: [CommonModule, RouterModule, SharedModule, IconComponent],
   exports: [DefaultLayoutComponent, HeaderComponent],
 })
 export class LayoutModule {}
