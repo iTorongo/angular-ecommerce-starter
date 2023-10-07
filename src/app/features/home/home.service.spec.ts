7c96dc10f322c78887097cfe983608b0f117c461
Arnaf Aziz Torongo <arnafaziz@gmail.com>
2023-10-07 23:37:24 +0200

    test: add unit test of product and home service

--- src/app/features/home/home.service.spec.ts
+++ src/app/features/home/home.service.spec.ts
@@ -1,19 +1,37 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
 import { HomeService } from './home.service';
 
 describe('HomeService', () => {
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
   let service: HomeService;
+  const MOCK_PRODUCTS: any = [
+    { id: 1, title: 'A' },
+    { id: 2, title: 'B' },
+    { id: 3, title: 'C' },
+  ];
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
-    service = TestBed.inject(HomeService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new HomeService(httpClientSpy);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return specific number of feature products', () => {
+    httpClientSpy.get.and.returnValue(of(MOCK_PRODUCTS.slice(0, 2)));
+
+    service.getFeaturedProducts(2).subscribe((products: any) => {
+      expect(products.length).toEqual(2);
+    });
+    expect(httpClientSpy.get).toHaveBeenCalledTimes(1);
   });
 });
--- src/app/features/products/products.service.spec.ts
+++ src/app/features/products/products.service.spec.ts
@@ -2,18 +2,46 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { ProductsService } from './products.service';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
 describe('ProductsService', () => {
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
   let service: ProductsService;
+  const MOCK_PRODUCTS: any = [
+    { id: 1, title: 'A' },
+    { id: 2, title: 'B' },
+  ];
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
-    service = TestBed.inject(ProductsService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new ProductsService(httpClientSpy);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return all expected products', () => {
+    httpClientSpy.get.and.returnValue(of(MOCK_PRODUCTS));
+
+    service.getProducts().subscribe((products: any) => {
+      expect(products).toEqual(MOCK_PRODUCTS);
+      expect(products.length).toEqual(MOCK_PRODUCTS.length);
+    });
+    expect(httpClientSpy.get).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return a single product with specific id', () => {
+    httpClientSpy.get.and.returnValue(of(MOCK_PRODUCTS[0]));
+
+    service.getProduct(1).subscribe((product: any) => {
+      expect(product.id).toEqual(MOCK_PRODUCTS[0].id);
+      expect(product.title).toEqual(MOCK_PRODUCTS[0].title);
+    });
+    expect(httpClientSpy.get).toHaveBeenCalledTimes(1);
   });
 });
