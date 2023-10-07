848501bb04ac1340b39a596b235615735cea0c66
Arnaf Aziz Torongo <arnafaziz@gmail.com>
2023-10-07 23:36:44 +0200

    test: add test case for turncate pipe

--- src/app/shared/pipes/truncate.pipe.spec.ts
+++ src/app/shared/pipes/truncate.pipe.spec.ts
@@ -1,8 +1,24 @@
 import { TruncatePipe } from './truncate.pipe';
 
 describe('TruncatePipe', () => {
-  it('create an instance', () => {
-    const pipe = new TruncatePipe();
-    expect(pipe).toBeTruthy();
+  let pipe: TruncatePipe;
+  beforeEach(() => {
+    pipe = new TruncatePipe();
+  });
+
+  it('should returns specific length string with ...', () => {
+    const truncated = pipe.transform('Lorem ipsum tolem', 5);
+    expect(truncated.length).toEqual(8);
+  });
+
+  it('should returns specific length string', () => {
+    const truncated = pipe.transform('Lorem ipsum tolem', 5, '');
+    expect(truncated.length).toEqual(5);
+    expect(truncated).toEqual('Lorem');
+  });
+
+  it('should returns actual text if provided text length is less than limit', () => {
+    const truncated = pipe.transform('Lorem', 6);
+    expect(truncated).toEqual('Lorem');
   });
 });
