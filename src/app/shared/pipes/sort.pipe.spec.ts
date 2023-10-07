0a3b6344a0a30241c13c10d0320c4b1113dcf0ee
Arnaf Aziz Torongo <arnafaziz@gmail.com>
2023-10-07 23:48:20 +0200

    test: add test case of sort pipe

--- src/app/shared/pipes/sort.pipe.spec.ts
+++ src/app/shared/pipes/sort.pipe.spec.ts
@@ -1,8 +1,16 @@
 import { SortPipe } from './sort.pipe';
 
 describe('SortPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SortPipe();
-    expect(pipe).toBeTruthy();
+  let pipe: SortPipe;
+  beforeEach(() => {
+    pipe = new SortPipe();
+  });
+
+  it('should returns sorted array based on given field with ascending order', () => {
+    const sorted = pipe.transform(
+      [{ price: 2 }, { price: 5 }, { price: 1 }],
+      'price'
+    );
+    expect(sorted).toEqual([{ price: 1 }, { price: 2 }, { price: 5 }]);
   });
 });
