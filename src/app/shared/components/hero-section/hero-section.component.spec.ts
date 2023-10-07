e27b40cf17fcfc3e6f14843d6df79a071c63cee1
Arnaf Aziz Torongo <arnafaziz@gmail.com>
2023-10-08 00:07:19 +0200

    test: add navigation test for hero action button

--- src/app/shared/components/hero-section/hero-section.component.spec.ts
+++ src/app/shared/components/hero-section/hero-section.component.spec.ts
@@ -1,6 +1,8 @@
-import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { ButtonComponent } from '***REMOVED***app/standalone/components';
 
 import { HeroSectionComponent } from './hero-section.component';
 
@@ -11,7 +13,7 @@ describe('HeroSectionComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [HeroSectionComponent],
-      schemas: [NO_ERRORS_SCHEMA],
+      imports: [ButtonComponent],
     });
     fixture = TestBed.createComponent(HeroSectionComponent);
     component = fixture.componentInstance;
@@ -27,5 +29,16 @@ describe('HeroSectionComponent', () => {
     const h1: HTMLElement = de.nativeElement;
 
     expect(h1.textContent).toEqual('Discover the Joy of Shopping with Us!');
+  });
+
+  it('should navigate to products place on clicking "Shop Now" button', () => {
+    const router = TestBed.get(Router);
+    const spy = spyOn(router, 'navigate');
+
+    const de: DebugElement = fixture.debugElement.query(By.css('.btn'));
+    const button: HTMLElement = de.nativeElement;
+    button.click();
+
+    expect(router.navigate).toHaveBeenCalledWith(['products']);
   });
 });
